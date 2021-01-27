763cc95e397f232dee6ccea1cbfae95591b14726
David Walker <david_walker1@hotmail.co.uk>
2021-01-27 15:13:29 +0100

    add Color Box

--- src/Palette.js
+++ src/Palette.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+import './Palette.css';
 
 class Palette extends Component {
 	render() {
+		const colorBoxes = this.props.colors.map((color, i) => (
+			<ColorBox background={color.color} key={i} name={color.name} />
+		));
 		return (
 			<div className="Palette">
 				{/* Navbar goes here */}
-				<div className="Palette-colors">{/* bunch of color boxes here */}</div>
+				<div className="Palette-colors">{colorBoxes}</div>
+				{/* footer eventually */}
 			</div>
 		);
 	}
