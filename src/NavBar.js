5f969229eec2a6d1193eff3f6728ab65f4f3fa63
David Walker <david_walker1@hotmail.co.uk>
2021-01-28 22:11:07 +0100

    add react router and palette list page including links

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import Snackbar from '@material-ui/core/Snackbar';
@@ -28,7 +29,7 @@ class NavBar extends Component {
 		return (
 			<header className="NavBar">
 				<div className="logo">
-					<a href="#">reactcolorpicker</a>
+					<Link to="/">reactcolorpicker</Link>
 				</div>
 				<div className="slider-container">
 					<span>Level: {level}</span>
---
+++ src/PaletteList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class PaletteList extends Component {
+	render() {
+		const palettes = this.props.palettes.map((palette) => (
+			<p>
+				<Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
+			</p>
+		));
+		return (
+			<div>
+				<h1>React Colors</h1>
+				{palettes}
+			</div>
+		);
+	}
+}
+
+export default PaletteList;
