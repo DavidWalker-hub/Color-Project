2d14df18d7558d851de0ebfb8f8d3d0df0741ebe
David Walker <david_walker1@hotmail.co.uk>
2021-01-28 18:53:13 +0100

    add snackbar and position select

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './NavBar.css';
@@ -8,12 +11,16 @@ import './NavBar.css';
 class NavBar extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { format: 'hex' };
-		this.handleChange = this.handleChange.bind(this);
+		this.state = { format: 'hex', open: false };
+		this.handleFormatChange = this.handleFormatChange.bind(this);
+		this.closeSnackbar = this.closeSnackbar.bind(this);
 	}
-	handleChange(evt) {
-		this.setState({ format: evt.target.value });
+	handleFormatChange(evt) {
+		this.setState({ format: evt.target.value, open: true });
 		this.props.handleChange(evt.target.value);
+	}
+	closeSnackbar() {
+		this.setState({ open: false });
 	}
 	render() {
 		const { level, changeLevel } = this.props;
@@ -30,12 +37,34 @@ class NavBar extends Component {
 					</div>
 				</div>
 				<div className="select-container">
-					<Select value={format} onChange={this.handleChange}>
+					<Select value={format} onChange={this.handleFormatChange}>
 						<MenuItem value="hex">HEX - #ffffff</MenuItem>
 						<MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
 						<MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
 					</Select>
 				</div>
+				<Snackbar
+					anchorOrigin={{
+						vertical: 'bottom',
+						horizontal: 'left'
+					}}
+					open={this.state.open}
+					autoHideDuration={4000}
+					message={<span id="message-id">Format changed! - {format.toUpperCase()}</span>}
+					ContentProps={{ 'aria-describedby': 'message-id' }}
+					onClose={this.closeSnackbar}
+					action={[
+						<IconButton
+							size="small"
+							aria-label="close"
+							key="close"
+							color="inherit"
+							onClick={this.closeSnackbar}
+						>
+							<CloseIcon fontSize="small" />
+						</IconButton>
+					]}
+				/>
 			</header>
 		);
 	}
