72ab2750d1ec817e1be6c460700ac06c43608b9d
David Walker <david_walker1@hotmail.co.uk>
2021-02-03 20:34:05 +0100

    add draggable color box without drag and drop

---
+++ src/DraggableColorBox.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+	root: {
+		width: '20%',
+		height: '25%',
+		margin: '0 auto',
+		display: 'inline-block',
+		position: 'relative',
+		cursor: 'pointer',
+		/* -3.5px in solution */
+		marginBottom: '-4px'
+	}
+};
+
+function Draggablecolorbox(props) {
+	const { classes } = props;
+	return (
+		<div className={classes.root} style={{ backgroundColor: props.color }}>
+			{props.color}
+		</div>
+	);
+}
+export default withStyles(styles)(Draggablecolorbox);
